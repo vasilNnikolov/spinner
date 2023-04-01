06092c91c5664a6da409fcd6f5099c838ea02cc8
Vasil Nikolov <vaskonikolov2003@gmail.com>
2023-04-01 20:54:02 +0300

    split project into multiple files

---
+++ src/constants.rs
@@ -0,0 +1,5 @@
+use crate::math::*;
+pub const FOV: f32 = 0.4; // horizontal field of view in radians
+pub const H_W_RATIO: f32 = 2.0;
+pub const HEIGHT: i32 = 70;
+pub const WIDTH: i32 = 100;
---
+++ src/math.rs
@@ -0,0 +1,54 @@
+pub trait Normalize {
+    fn normalise(self) -> Self;
+}
+
+impl Normalize for Vector {
+    fn normalise(self) -> Vector {
+        self / self.norm()
+    }
+}
+pub type Vector = nalgebra::SVector<f32, 3>;
+
+macro_rules! vector {
+    ($x: expr, $y:expr, $z: expr) => {{
+        Vector::from([$x as f32, $y as f32, $z as f32])
+    }};
+}
+pub(crate) use vector;
+
+#[cfg(test)]
+mod test_math {
+    use super::{Matrix, Normalize, Vector};
+    #[test]
+    fn add_arrays() {
+        let a = Vector::from_vec(vec![1., 2., 3.]);
+        let b = Vector::from_vec(vec![4., 2., 3.]);
+        let x = a + b;
+
+        // println!("{:?}", (&b.shape()).into_iter());
+        let res = Vector::from_vec(vec![5., 4., 6.]);
+        assert!(x == res);
+        println!("{}, {}", a, b);
+    }
+    #[test]
+    fn test_vector_macro() {
+        let a = vector!(1, 2, 3);
+        assert!(a == Vector::from_vec(vec![1., 2., 3.]))
+    }
+    #[test]
+    fn test_normalize_trait() {
+        let difference = vector!(3, 4, 5).normalise() - vector!(0.42426, 0.56568, 0.7071);
+        assert!(difference.dot(&difference) < 0.001)
+    }
+    #[test]
+    fn test_matrix_from_col() {
+        let A = super::matrix_from_columns([vector!(2, 0, 0), vector!(0, 2, 0), vector!(0, 0, 2)]);
+        assert!(A * A == 2 as f32 * A)
+    }
+}
+
+pub type Matrix = nalgebra::SMatrix<f32, 3, 3>;
+
+pub fn matrix_from_columns(columns: [Vector; 3]) -> Matrix {
+    Matrix::from_columns(&columns)
+}
