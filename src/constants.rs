2a16ef4bb72f6c35c05f409f0b176764f2638202
Vasil Nikolov <vaskonikolov2003@gmail.com>
2023-04-10 00:56:37 +0300

    make viewing field larger

--- src/constants.rs
+++ src/constants.rs
@@ -2,5 +2,5 @@
 pub const FOV: f32 = 0.8;
 /// the ratio of height to width of a typical terminal
 pub const H_W_RATIO: f32 = 2.0;
-pub const HEIGHT: i32 = 50;
-pub const WIDTH: i32 = 180;
+pub const HEIGHT: i32 = 100;
+pub const WIDTH: i32 = 360;
