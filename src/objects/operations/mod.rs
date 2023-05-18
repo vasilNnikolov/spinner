1d3a3b27d5e611c889a59bcdd53095a61a8cfe9e
vasil <vaskonikolov2003@gmail.com>
2023-05-18 19:29:09 +0300

    add description for ObjectOperation trait

--- src/objects/operations/mod.rs
+++ src/objects/operations/mod.rs
@@ -5,6 +5,8 @@ pub mod soft_union;
 pub mod union;
 pub mod utility_functions;
 
+/// This trait specifies that the struct implementing it is an operation, i.e. it takes one or more `Object3D` and
+/// produces another `Object3D` in a way specific to the type of operation.
 pub trait ObjectOperation: Object3D {
     fn from_objects_default(objects: Vec<Box<dyn Object3D>>) -> Self;
 }
