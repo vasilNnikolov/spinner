253015a12f5435ae6cf2d52bf82e941d89c15c9c
vasil <vaskonikolov2003@gmail.com>
2023-05-18 20:22:20 +0300

    impl CompoundObject for cuboid

--- src/objects/compound_objects/cuboid.rs
+++ src/objects/compound_objects/cuboid.rs
@@ -27,25 +27,11 @@ impl Cuboid {
     }
 }
 
-impl SDF_Centered for Cuboid {
-    fn signed_distance_function_centered(&self, position: &Vector) -> f32 {
-        self.components.signed_distance_function_centered(position)
+impl CompoundObject for Cuboid {
+    fn get_components(&self) -> &dyn Object3D {
+        &self.components
     }
-}
-
-impl Orientable for Cuboid {
-    fn get_center(&self) -> &Vector {
-        self.components.get_center()
-    }
-    fn get_inverse_orientation_matrix(&self) -> &Matrix {
-        self.components.get_inverse_orientation_matrix()
-    }
-}
-impl OrientableMut for Cuboid {
-    fn get_center_mut(&mut self) -> &mut Vector {
-        self.components.get_center_mut()
-    }
-    fn get_inverse_orientation_matrix_mut(&mut self) -> &mut Matrix {
-        self.components.get_inverse_orientation_matrix_mut()
+    fn get_components_mut(&mut self) -> &mut dyn Object3D {
+        &mut self.components
     }
 }
